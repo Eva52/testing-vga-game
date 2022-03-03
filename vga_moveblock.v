6cead0d8930bd16fe3ae50a1e7632bb4c5cae914
Jared Chan <jaredchan42@gmail.com>
2022-03-03 09:57:07 -0800

    Bluetooth support

--- vga_moveblock.v
+++ vga_moveblock.v
@@ -3,7 +3,8 @@ module vga_moveblock (input CLOCK_50, input [3:0] KEY,
              output [6:0] HEX0, output [6:0] HEX1, output [6:0] HEX2,
              output [6:0] HEX3, output [6:0] HEX4, output [6:0] HEX5,
              output [7:0] VGA_R, output [7:0] VGA_G, output [7:0] VGA_B,
-             output VGA_HS, output VGA_VS, output VGA_CLK);
+             output VGA_HS, output VGA_VS, output VGA_CLK,
+			 input [35:0] GPIO_0);
 	
 	wire						locked;
 	wire			[23:0]		vga_data;
@@ -24,16 +25,29 @@ module vga_moveblock (input CLOCK_50, input [3:0] KEY,
 		  assign VGA_B=vga_rgb[23:16];
         
 	vga_pll my_pll_inst(
-			.rst(SW[0]),
+			.rst(SW[0]|bt_reset_out),
 			.refclk(CLOCK_50),
 			.outclk_0(VGA_CLK),
 			.locked(locked)
+	);
+
+	wire [4:0] bt_o;
+	wire [4:0] bt_out = {1'b0, ~bt_o[3], ~bt_o[2], ~bt_o[1], ~bt_o[0]};
+	wire bt_reset_out;
+	DE1_SOC_Bluetooth_Slave bt_inst(
+		.CLOCK_50(CLOCK_50),
+		.RESET(1),
+		.KEY_out(bt_o),
+		.reset_out(bt_reset_out),
+		.SW(0),
+		.GPIO_0(GPIO_0)
 	);
 	
 	key_filter key_filter1_inst(
 			.clk(CLOCK_50),
 			.rst_n(locked),
-			.key_in(KEY[0]),
+			//.key_in(KEY[0]),
+			.key_in(KEY[0]&bt_out[0]),
 			
 			.nege_flag(key_flag1)
 	);
@@ -41,7 +55,8 @@ module vga_moveblock (input CLOCK_50, input [3:0] KEY,
 	key_filter key_filter2_inst(
 			.clk(CLOCK_50),
 			.rst_n(locked),
-			.key_in(KEY[1]),
+			//.key_in(KEY[1]),
+			.key_in(KEY[1]&bt_out[1]),
 			
 			.nege_flag(key_flag2)
 	);
@@ -49,7 +64,8 @@ module vga_moveblock (input CLOCK_50, input [3:0] KEY,
 	key_filter key_filter3_inst(
 			.clk(CLOCK_50),
 			.rst_n(locked),
-			.key_in(KEY[2]),
+			//.key_in(KEY[2]),
+			.key_in(KEY[2]&bt_out[2]),
 			
 			.nege_flag(key_flag3)
 	);
@@ -57,7 +73,8 @@ module vga_moveblock (input CLOCK_50, input [3:0] KEY,
 	key_filter key_filter4_inst(
 			.clk(CLOCK_50),
 			.rst_n(locked),
-			.key_in(KEY[3]),
+			//.key_in(KEY[3]),
+			.key_in(KEY[3]&bt_out[3]),
 			
 			.nege_flag(key_flag4)
 	);
